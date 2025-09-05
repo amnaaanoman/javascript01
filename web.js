dfa4b09ca9c1bac291c82ef07fd024e22ca590fb
amnanoman <bintenoman418@gmail.com>
2025-09-05 03:33:40 -0700

    javascript pdfs

---
+++ web.js
@@ -0,0 +1,222 @@
+// chapter 01
+// alert
+// i)
+alert("hello my friends  thanks for coming")
+
+// ii)
+alert("Error! Please enter a valid password ")
+
+// iii)
+alert("Welcome to JS Land...\nHappy Coding!")
+
+// iv)
+ alert("Welcome to JS Land")
+
+alert("Happy Coding")
+// v)
+alert("Hello... I can run JS through my web browser's console ")
+
+// chapter 1 end 
+
+// chapter 02
+// // variable and string
+// question 1
+// i)
+var username = "Amna";
+console.log(username); 
+// question 2
+// a)
+var myName = "Amna";
+alert(myName); 
+console.log(myName);
+
+// b)
+var fullname = "Amnanoman";
+alert(fullname);
+console.log(fullname);
+
+// question 3
+// a)
+var message = "Hello World!";
+console.log(message);
+// b)
+var titled = "Hello World";
+console.log(titled);
+alert(titled);
+// c)
+var message = "Hello World";
+alert (message);
+
+// question 4
+var studentname = "Jhone Doe";
+alert (studentname);
+console.log(studentname);
+
+var age = "15 years old";
+alert (age);
+console.log(age);
+
+var studentcertified = "Certified Mobile Application Development"
+alert (studentcertified);
+console.log(studentcertified);
+
+// question 5
+
+var  pizza = (" Pizza\n Pizz\n Piz\n Pi \n P");
+alert (pizza);
+
+
+// question 6
+var email = ("my email address is amnanoman@gmail.com")
+alert (email)
+console.log(email)
+
+// question 7
+var book =("I am trying to learn from the book A smarter\nway to learn Javascript");
+alert (book);
+console.log(book);
+
+
+// question 8
+document.write("Yah! I can write HTML content through Javascript");
+
+
+// question 9
+ var unicode = ("▂ ▂ ▂ ▂ ▂ ▂ ▂ ▂ ▂ ▂ஜ۩۞۩ஜ▂ ▂ ▂ ▂ ▂ ▂ ▂ ▂ ▂ ▂");
+alert (unicode);
+console.log(unicode);
+document.write(unicode);
+
+// chapter 2 end
+
+// chapter 3 
+
+// question 1
+
+var visits = ("You have visited this site 14 times");
+alert(visits)
+
+// question 2
+var birthyear = ("My birth year is 2008 \nData type of my declared variable is number");
+alert(birthyear);
+document.write("My birth year is 2008 <br>Data type of my declared variable is number");
+console.log(birthyear);
+
+//  question 3
+var visitor;
+document.write(" “<b>John </b> <b>Doe</b> ordered <b>5 T-shirt(s)</b> on XYZ Clothing store”. ")
+// chapter 3 end
+
+
+// chapter 4 
+
+// question 1
+    var names = "Amna", fullname = "noman", city = "karachi";
+    console.log(names , fullname , city);
+
+// question 2
+// 5 legal name
+var $haha ;
+var _undescore;
+var age = 16;
+var its_me;
+var name  = "hamza";
+
+// 5 illegal name
+
+
+// ?
+// -
+// #
+var myname;
+var hamza
+
+// question 3
+ document.write("<b> Rules for naming JS variables</b> <br>")
+ document.write(" <br>Variable names can only contain,numbers, $ and _.  For example : $my_1stVariable <br> ")
+  document.write("Varaiables must begin with a letter , $ and _.  For example : $name, _name or name <br>");
+    document.write("Varaiables names are case sensitive <br>");
+    document.write("Varaiables names should not be JS keywords");  
+
+// chapter 4 end
+
+// chapter 5
+
+//  add
+document.write("Sum of 3 and 5 is 8 ")
+    var a = 5 , b =3 , c = a+b;
+    alert(c);
+  
+     
+//    subtraction
+      var a = 5 , b =3 , c = a-b;
+      document.write(c);
+    alert(c);
+
+    // multiplication
+    var a = 5 , b =3 , c = a*b;
+      document.write(c);
+    alert(c);
+    
+    // modulus
+      var a = 5 , b =3 , c = a%b;
+      document.write(c);
+    alert(c);
+    
+    // division
+    var a = 5 , b =3 , c = a/b;
+    document.write(c);
+    alert(c)
+//  question 3
+// a)
+var  a;
+// b)
+var number;
+document.write("Value after variable declaration is:  " + number+"<br>");
+// c)
+var number = 3+2 ;
+document.write("Initial value is " + number +"<br>" ); 
+// d)
+var number = 5; 
+number++;         
+document.write("Value after increment is: " + number+"<br>");
+// e)
+var x = "7"
+var y = 6;
+var z = x+y;
+alert(z);
+
+// f)
+ var number = 13 ;
+ document.write("Value after additon is: " + number+"<br>" ); 
+// g)
+var number = 0 ;
+document.write("The remainder is :" + number ); 
+
+//  question 4
+document.write("Total cost to buy ")
+var numbers = 5
+document.write(numbers)
+var number = 5+2994;
+document.write(" tickets to a  movie is ")
+number++
+document.write(number++ + "PKR")
+
+//  question 5
+var number = 4;
+
+document.write("Multiplication Table of " + number + "<br><br>");
+
+document.write(number + " x 1 = " + number * 1 + "<br>");
+document.write(number + " x 2 = " + number * 2 + "<br>");
+document.write(number + " x 3 = " + number * 3 + "<br>");
+document.write(number + " x 4 = " + number * 4 + "<br>");
+document.write(number + " x 5 = " + number * 5 + "<br>");
+document.write(number + " x 6 = " + number * 6 + "<br>");
+document.write(number + " x 7 = " + number * 7 + "<br>");
+document.write(number + " x 8 = " + number * 8 + "<br>");
+document.write(number + " x 9 = " + number * 9 + "<br>");
+document.write(number + " x 10 = " + number * 10 + "<br>");
+console.log(number)
+
+
